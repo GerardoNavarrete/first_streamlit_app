dc17cf891f48ee1f874589289c9da1d22a4074df
GerardoNavarrete <132381888+GerardoNavarrete@users.noreply.github.com>
2023-08-29 09:32:50 -0600

    Update streamlit_app.py
    -Added a new item
    -Included stickers
    -Added new header for the menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,10 @@
 import streamlit
 
 streamlit.title("My Parents New Healthy Dinner")
-streamlit.header("Breakfast Menu")
-streamlit.text("OMEGA 3 & Blueberry Oatmeal")
-streamlit.text("kale, Spinach & Smoothie")
-streamlit.text("Hard-Boiled Free-Range Egg")
+streamlit.header"Breakfast Menu")
+streamlit.text("🥣OMEGA 3 & Blueberry Oatmeal")
+streamlit.text("🥗kale, Spinach & Smoothie")
+streamlit.text("🐔Hard-Boiled Free-Range Egg")
+stremlit.text("🥑🍞Avocado Toast")
+
+streamlit.header("🍌🥭 Build Your Own Fruit Smoothie 🥝🍇")
