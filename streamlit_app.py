4c8e30bef891c65eaa7791b0b16396a065b7b246
GerardoNavarrete <132381888+GerardoNavarrete@users.noreply.github.com>
2023-08-29 09:36:16 -0600

    Update streamlit_app.py
    -Fixed a parenthesis error in "breakfast menu"

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit
 
 streamlit.title("My Parents New Healthy Dinner")
-streamlit.header"Breakfast Menu")
+streamlit.header("Breakfast Menu")
 streamlit.text("🥣OMEGA 3 & Blueberry Oatmeal")
 streamlit.text("🥗kale, Spinach & Smoothie")
 streamlit.text("🐔Hard-Boiled Free-Range Egg")
